16d502be058e45c39593eb267bb6f9ecbbdf2087
alexsandr <drevs.aleksandr@gmail.com>
2020-12-19 16:11:47 +0600

    сделал правки

--- source/js/main-nav.js
+++ source/js/main-nav.js
@@ -1,20 +1,21 @@
 var navMain = document.querySelector('.main-nav');
 var navToggle = document.querySelector('.page-header__toggle');
 
-navToggle.addEventListener('click', function() {
-  if (navMain.classList.contains('main-nav--closed')) {
-    navMain.classList.remove('main-nav--closed');
-    navMain.classList.add('main-nav--opened');
-  } else {
-      navMain.classList.add('main-nav--closed');
-      navMain.classList.remove('main-nav--opened');
-  }
+navMain.classList.remove('main-nav--nojs');
+navToggle.classList.remove('page-header__toggle--nojs');
 
-  if (navToggle.classList.contains('page-header__toggle--closed')) {
-    navToggle.classList.remove('page-header__toggle--closed');
-    navToggle.classList.add('page-header__toggle--opened');
-  } else {
-      navToggle.classList.add('page-header__toggle--closed');
-      navToggle.classList.remove('page-header__toggle--opened');
-  }
-});
+if(document.querySelector('.main-nav')) {
+  navToggle.addEventListener('click', function() {
+    if (navMain.classList.contains('main-nav--closed')) {
+      navMain.classList.remove('main-nav--closed');
+      navMain.classList.add('main-nav--opened');
+      navToggle.classList.remove('page-header__toggle--closed');
+      navToggle.classList.add('page-header__toggle--opened');
+    } else {
+        navMain.classList.add('main-nav--closed');
+        navMain.classList.remove('main-nav--opened');
+        navToggle.classList.add('page-header__toggle--closed');
+        navToggle.classList.remove('page-header__toggle--opened');
+      }
+    });
+}
